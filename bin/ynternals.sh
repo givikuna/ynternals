c1a2f0de27a9ae13830a6ce63da5b8d811a7f16d
givikuna <gvtsvariani@gmail.com>
2026-09-19 19:41:05 -0400

    -s -> -rs

--- bin/ynternals.sh
+++ bin/ynternals.sh
@@ -9,7 +9,7 @@ if [ ! -f "$KEYFILE" ]; then
 fi
 
 echo -n "enter secret data: "
-read -s SECRET_DATA
+read -rs SECRET_DATA
 echo
 
 echo -n "$SECRET_DATA" | openssl enc -aes-256-cbc -pbkdf2 -salt -a -pass "file:$KEYFILE"
